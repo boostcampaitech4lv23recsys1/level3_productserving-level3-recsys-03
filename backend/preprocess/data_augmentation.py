b26667b12e3dc09ef68fb5aa703cf3aacd087f59
woong <hiidy115@gmail.com>
2023-02-07 07:31:25 +0000

    ♻️ Refactoring augmentation baseline (#44)

--- backend/preprocess/data_augmentation.py
+++ backend/preprocess/data_augmentation.py
@@ -5,57 +5,8 @@ from sklearn.metrics.pairwise import linear_kernel
 
 from tqdm import tqdm
 import time
+from ..app.firebase_db import load_database, get_user_solved, get_user_solved_seq, get_dataframe, get_full_problemCode, get_problem_similar
 
-
-def to_adv(df):
-    for idx, row in df.iterrows():
-        i = row['assessmentItemID']
-        if '_' in i[-2:]:
-            i = i[:-2] + i[-1:].zfill(2)
-            # i[-1:]
-            # i = i.replace(i[-2:], i[-1:].zfill(2))
-            # print(i)
-        i = i.replace('_' , "")
-        i = i.replace('deep', 'advanced')
-        df.loc[idx, 'assessmentItemID'] = i
-
-    return df
-
-
-def to_basic(df):
-    for idx, row in df.iterrows():
-        i = row['assessmentItemID']
-        if '_' in i[-2:]:
-            i = i[:-2] + i[-1:].zfill(2)
-            # i[-1:]
-            # i = i.replace(i[-2:], i[-1:].zfill(2))
-            # print(i)
-        i = i.replace('_' , "")
-        df.loc[idx, 'assessmentItemID'] = i
-
-    return df
-
-def get_idx(df):
-    idx = pd.Series(df.index, index=df['assessmentItemID']).drop_duplicates() # 문제 인덱스
-    return idx
-
-
-def cal_sim(df):
-    tfidf = TfidfVectorizer() #Tfidf 벡터화
-    tfidf_matrix = tfidf.fit_transform(df['keyword'])
-    cos_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-    
-    return cos_sim
-
-def tfidf_recommender(id, df, top_n : int):
-    assessmentID_idx = get_idx(df)[id]
-    sim_score = list(enumerate(cal_sim(df)[assessmentID_idx]))
-    sim_score = sorted(sim_score, key=lambda x: x[1], reverse = True)
-    sim_score = sim_score[1:(top_n + 1)] # 자기 제외하고 유사도 높은 순으로 Top-10 추천
-    res_idx = [i[0]  for i in sim_score]
-    tmp = pd.DataFrame(df['assessmentItemID'].iloc[res_idx])
-    tmp = tmp['assessmentItemID'].values.tolist()
-    return tmp
 
 def duplicate_data(idx, df, rec):
     tmp1 = df[df['problemCode']==idx].reset_index(drop=True)
@@ -71,34 +22,25 @@ def duplicate_data(idx, df, rec):
     return dup
 
 def main():
-    basic = pd.read_csv('./data/data_basic.csv')
-    deep = pd.read_csv('./data/data_deep.csv')
 
-    basic = to_basic(basic)
-    deep = to_adv(deep)
-
+    db = load_database()
+    df = get_dataframe(db)
     user_idx = df['userUID'].unique()
     
     # data augementaion
 
-    print("start preprocessing")
+    print("start augmentation")
+
     for user in tqdm(user_idx):
         tmp = df[df['userUID'] == user]
         solved_idx = tmp['problemCode'].unique()
         for solved in solved_idx:
-            if 'basic' in solved:
-                rec_list = tfidf_recommender(solved, basic, 2)
-                for i in rec_list:
-                    if i in solved_idx:
-                        continue
-                    df = pd.concat([df,duplicate_data(solved, tmp, i) ])
-
-            else:
-                rec_list = tfidf_recommender(solved, deep, 2)
-                for i in rec_list:
-                    if i in solved_idx:
-                        continue
-                    df = pd.concat([df,duplicate_data(solved, tmp, i) ])
+            rec_list = get_problem_similar(db, solved)[:2]  # Top-2개 복제
+            for i in rec_list:
+                if i in solved_idx:
+                    continue
+                df = pd.concat([df,duplicate_data(solved, tmp, i) ])
+    print("Done!")
 
 
 
